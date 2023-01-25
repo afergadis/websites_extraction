36cd02d2ecec3476e2cd80a164c4dce1eb32fb58
C.Borovilou <C.Borovilou@di.uoa.gr>
2023-01-26 00:49:03 +0200

    test 1

---
+++ venv/Scripts/Crawler_pdfs.py
@@ -0,0 +1,286 @@
+import os, requests, random, json
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+# from lxml import etree
+from collections import Counter
+from tqdm import tqdm
+from my_lang_detect import lang_detect_on_txt_par
+from timeout import timeout
+from global_handlers import filter_urls_2, fix_the_html, filter_pdf_urls
+from pathlib import Path
+import shutil
+import argparse
+
+def string_found_in_text(word_dict, l_text):
+    found = False
+    for l_string in word_dict:
+        if l_string in l_text or\
+            l_string.lower() in l_text or\
+            l_string.upper() in l_text or\
+            l_string.capitalize() in l_text:
+            print('Evrika {}'.format(l_string))         #del me at final version
+            found = True
+            break
+    return found
+
+def clear_url(url):
+    while(url.endswith('/')):
+        url = url[:-1]
+    if(url.startswith('www')):
+        url = 'http://{}'.format(url)
+    if not (url.startswith('http://') or url.startswith('https://')):
+        try:
+            page    = requests.get('http://{}'.format(url))
+            url     = 'http://{}'.format(url)
+        except:
+            url     = 'https://{}'.format(url)
+    # if(url.endswith(")):
+    #     print('errorrrrrs,,,,!!!!')
+    url.replace("'",'\'')
+    # if (url.startswith(")):
+    #     print('Polllaaa errorrrrrs,,,,!!!!')
+    return url
+
+def get_languages(soup):
+    page_langs      = []
+    all_text        = soup.text.strip()
+    paragraphs      = all_text.split('\n\n')
+    paragraphs      = [p for p in paragraphs if len(p.strip()) > 0]
+    long_paragraphs = [p for p in paragraphs if len(p.strip()) > 100]
+    if len(long_paragraphs) > 0:
+        for lp in long_paragraphs:
+            try:
+                le_lang = lang_detect_on_txt_par(lp)
+                page_langs.append(le_lang)
+            except:
+                le_lang = 'unsure'
+                page_langs.append(le_lang)
+    else:
+        le_lang = 'unsure'
+        page_langs.append(le_lang)
+    return Counter(page_langs)
+
+def get_the_html(url):
+    print(url)
+    page = requests.get(url)
+
+    htmltext = page.text
+    print(page)
+
+    print(page.reason)
+    return page
+
+def download_pdf(url, pdf_name, dir,domain):
+
+    print("Trying downloading file: ", pdf_name)
+
+    # Get response object for link
+    url_str = url['href']
+    if (url_str.startswith('/')):           #case when webpage is dynamically build in html#
+        if ('www.' in url_str):
+            print('check here please''{}\n'.format(url_str))
+            # downl_status = 'Failed'
+            return None
+        else:
+            url_str =  domain + url_str
+
+    response = requests.get(url_str)
+
+    if response.status_code == 200:
+        dir_f = os.path.join(dir, pdf_name + ".pdf")
+        # Write content in pdf file
+        pdf = open(dir_f, 'wb')
+        pdf.write(response.content)
+        pdf.close()
+
+        print("File ", pdf_name, " downloaded")
+        downl_status = 'Ok'
+    else:
+        downl_status = 'Failed'
+    return downl_status;
+
+
+#  ------------ ------------ ------------ ------------ ------------ ------------ ------------ ------------ ------------
+#  ------------ ------------ ------------ ------------ ------------ ------------ ------------ ------------ ------------
+
+
+parser = argparse.ArgumentParser()
+# parser.add_argument("--max_pages_to_visit", type=int, default=1000, help="Maximum pages to visit.",     required=False)
+parser.add_argument("--max_pages_to_visit", type=int, default=1000, help="Maximum pages to visit.",     required=False)
+
+parser.add_argument("--inpath",             type=str,               help="input json file.",            required=True)
+parser.add_argument("--out_dir",            type=str,               help="output root directory path.", required=True)
+
+
+inp= r"C:\Users\c.borovilou\Desktop\MSc\διπλωματική\GenericWebPageCrawler-master\GenericWebPageCrawler-master\karfoma2.json"     #admie
+inp= r"C:\Users\c.borovilou\Desktop\MSc\διπλωματική\karfoma3.json"
+# inp= r"C:\Users\c.borovilou\Desktop\MSc\διπλωματική\GenericWebPageCrawler-master\GenericWebPageCrawler-master\dnb_sample.json"     #admie
+# ot = r"C:\Users\c.borovilou\Desktop\MSc\διπλωματική\GenericWebPageCrawler-master\GenericWebPageCrawler-master\ott"
+ot = r"C:\Users\c.borovilou\Desktop\MSc\διπλωματική\GenericWebPageCrawler-master\GenericWebPageCrawler-master\eres"
+
+with open(os.path.join(ot, 'ESG_files_found.txt'), 'w', encoding='utf-8') as f_esg:
+    f_esg.write("ESG files have been found at the following paths:\n")
+    f_esg.write("Path"+ '\t' +  'File_name' + '\t' + 'Download_Status' + '\n')
+    f_esg.close()
+# #argv = ['', ".\data2crawl.json", ".\eres"]
+# argv = ['', inp, ot]
+
+# args                = parser.parse_args()
+# inpath              = args.inpath
+# out_dir             = args.out_dir
+# max_pages_to_visit  = args.requests
+
+inpath = inp
+out_dir = ot
+max_pages_to_visit = 1000
+
+
+
+to_crawl    = json.load(open(inpath,'r',encoding='utf-8'))
+random.shuffle(to_crawl)
+
+pbar = tqdm(to_crawl)
+download_html = False
+
+for (pic_type, base_url, pic_id) in pbar:
+    page_dir = os.path.join(out_dir, pic_type)
+    pic_dir = os.path.join(out_dir, pic_type, pic_id)
+    url_domain = (urlparse(base_url).netloc).split(".")[1]
+    url_domain_full = urlparse(base_url).scheme + '://'  + urlparse(base_url).netloc
+    # >> PENDING TO UNCOMMENT - CB - START
+    # os.makedirs(pic_dir)
+
+    if not os.path.exists(pic_dir):
+        os.makedirs(pic_dir)
+    else:
+        # continue      ############## CHECK AGAIN 15.01.2023 ##################
+        print('do not make dir that exists:{}'.format(pic_dir))
+
+    # << PENDING TO UNCOMMENT - CB - END
+    ################################################
+    # base_url = clear_url(base_url)        pending
+    clean_url = base_url.replace("'", '\"')
+    pbar.set_description(base_url)
+    ################################################
+    already_visited = set()
+    to_visit        = [base_url]
+    ################################################
+    visited_counter = 0
+    ################################################
+    c = i = 0
+    if download_html == True:
+        pages_visited   = open(os.path.join(pic_dir, 'pages_visited.txt'),   'a', encoding='utf-8')
+        pages_error     = open(os.path.join(pic_dir, 'pages_error.txt'),     'a', encoding='utf-8')
+        pages_discarded = open(os.path.join(pic_dir, 'pages_discarded.txt'), 'a', encoding='utf-8')
+
+    # μπεσ ιστοσελιδα και παρε ολα τα html απο ο,τι url βρεις
+    # αν δε, βρεις τη λεξούλα esg, κοιτα μηπως καλείται κανενα ulr με pdf και κάνε το download
+    while(len(to_visit)>0):
+
+        pbar.set_description('{}:{}:{}'.format(os.path.join(pic_type, pic_id), base_url,len(to_visit)))
+        url         = to_visit[0]
+        to_visit    = to_visit[1:]
+        already_visited.add(url)
+
+        try:
+            page = get_the_html(url)
+
+        except:
+            print('Error for page: {}'.format(url))
+            if download_html == True:
+                pages_error.write('{}\n'.format(url))
+                continue
+        ################################################
+        soup = BeautifulSoup(page.text, "lxml")
+        page_langs  = get_languages(soup)
+
+        c       += 1
+        pic_page_dir = os.path.join(pic_dir, str(c))
+        if download_html == True:
+            if not os.path.exists(pic_page_dir):
+                os.makedirs(pic_page_dir)
+        ################################################
+
+            with open(os.path.join(pic_page_dir,'source_of_page.html'), 'w', encoding='utf-8') as fp:
+                fp.write(page.text)
+                fp.close()
+            with open(os.path.join(pic_page_dir,'webpage_visited.txt'), 'w', encoding='utf-8') as fp:
+                fp.write(url)
+                fp.close()
+            ################################################
+            with open(os.path.join(pic_page_dir,'languages.json'), 'w', encoding='utf-8') as of:
+                of.write(json.dumps(dict(page_langs), indent=4, sort_keys=False))
+                of.close()
+        ####################################################################################
+        ################################################        12.01.2023 >>
+        esg_dict = [
+        'ESG',
+        'ΒΙΩΣΙΜΗ ΑΝΑΠΤΥΞΗ',
+        'ΒΙΩΣΙΜΗΣ',
+        'ΒΙΩΣΙΜΌΤΗΤΑ',
+        'Environmental Social Governance',
+        'Sustainable',
+        'Sustainability', 'CSR'
+        ]
+
+        # string_of_interest = 'ESG'
+        esg_exists = string_found_in_text(esg_dict, soup.text)
+        ####    CAN BE IMPROVED USING A DICTIONARY >>
+        # if esg_exists == False:
+        #     string_of_interest = 'ΒΙΩΣΙΜΗ ΑΝΑΠΤΥΞΗ'
+        #     esg_exists = string_found_in_text(string_of_interest, soup.text)
+        # if esg_exists == False:
+        #     string_of_interest = 'Environmental Social Governance'
+        #     esg_exists = string_found_in_text(string_of_interest, soup.text)
+
+        if esg_exists == True:
+
+            pdf_dir = ''
+            # # -- Isolate pdf links & download them --
+            links = soup.find_all('a')
+            # for link in links:
+            #     if ('.pdf' in link.get('href', [])):
+            gen_links = ( link for link in links if ('.pdf' in link.get('href', [])) )
+            for link in gen_links:
+                pdf_dir = os.path.join(page_dir, 'esg_pdf')
+                if not os.path.exists(pdf_dir):
+                    os.makedirs(pdf_dir)
+                i += 1
+                # f_name  = string_of_interest + '_' + str(url_domain) + '_' + str(i)
+                f_name  = 'ESG' + '_' + str(url_domain) + '_' + str(i)
+
+                f_downl_status = download_pdf(link, f_name, pdf_dir, url_domain_full)
+                with open(os.path.join(ot, 'esg_filesfound.txt'), 'a', encoding='utf-8') as f_esg:
+                    f_esg.write(pdf_dir + '\t' + f_name +  '\t' + f_downl_status)
+                    f_esg.close()
+        ################################################        12.01.2023 <<
+        ####################################################################################
+
+        accepted_links, discarded_links = filter_urls_2(
+            base_url,
+            [
+                a.get('href')
+                for a in soup.findAll('a')
+            ]
+        )
+
+        to_visit = to_visit + list(accepted_links - set(to_visit) - already_visited)
+
+
+        if download_html == True:
+            pages_visited.write('{}\n'.format(url))
+            for disc_page in discarded_links:
+                pages_discarded.write('{}\n'.format(disc_page))
+        ################################################
+
+        visited_counter +=1
+        if(visited_counter >= max_pages_to_visit):
+            # print('Max pages to visit limit WAS exceeded. Download of site {} stopped'.format(clean_url))
+            print(f'Max pages to visit limit WAS exceeded. Download of site {clean_url} stopped')
+            break
+
+    if download_html == True:
+        pages_visited.close()
+        pages_discarded.close()
+        pages_error.close()
+
